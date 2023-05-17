fbf76b0278cc0a87d6e5f9d0d529d18954eebed7
Tannury <tannury.p.martins@gmail.com>
2023-05-17 17:06:00 +0000

    fixing errors

--- CA1_myApp/src/app/page1/page1.page.ts
+++ CA1_myApp/src/app/page1/page1.page.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page1.page.html',
   styleUrls: ['./page1.page.scss'],
 })
-export class Page1Page implements OnInit {
+export class Page1Page {
 
 
   facts = [
