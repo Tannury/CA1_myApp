440ca87705e0fd8903cd71ec29416582f2f2a823
Tannury <tannury.p.martins@gmail.com>
2023-05-17 16:11:15 +0000

    editing page 1

--- CA1_myApp/src/app/page1/page1.page.ts
+++ CA1_myApp/src/app/page1/page1.page.ts
@@ -11,18 +11,39 @@ export class Page1Page implements OnInit {
   facts = [
     {
       id: 1,
-      content: 'Fact 1: Lorem ipsum dolor sit amet.',
+      title: 'Saudade',
+      image: 'assets/images/saudade.jpg',
+      details: 'Interesting fact 1',
+      expanded: false,
     },
     {
       id: 2,
-      content: 'Fact 2: Consectetur adipiscing elit.',
+      title: 'Futebol',
+      image: 'assets/images/futebol.jpg',
+      details: 'Interesting fact 2',
+      expanded: false,
     },
-    // Add more facts as needed
+    {
+      id: 3,
+      title: 'Festa',
+      image: 'assets/images/festa.jpg',
+      details: 'Interesting fact 3',
+      expanded: false,
+    },
   ];
 
-  constructor() { }
+  selectedFact: any;
 
-  ngOnInit() {
+  toggleFactDetails(fact: any) {
+    fact.expanded = !fact.expanded;
+    this.selectedFact = fact.expanded ? fact : null;
+  }
+
+  generateRandomFact() {
+    const randomIndex = Math.floor(Math.random() * this.facts.length);
+    const randomFact = this.facts[randomIndex];
+    this.toggleFactDetails(randomFact);
+
   }
 
 }
